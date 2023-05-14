4fd83ad523b0c2eba8ea57c3bfe3488ad0aaff18
pausegales <pau.segales01@estudiant.upf.edu>
2023-05-14 14:27:25 +0200

    Reverb ellipses added

--- Source/ReverbPedal.cpp
+++ Source/ReverbPedal.cpp
@@ -40,7 +40,7 @@ void ReverbPedal::paint (juce::Graphics& g)
     g.setColour (juce::Colours::white);
     g.setFont (14.0f);
     
-    juce::Rectangle<int> topLeft, topRigth, bottomLeft, bottomRigth, bypass;
+    juce::Rectangle<int> topLeft, topRigth, bottomCenter;
     auto rectWidth = 10;
     auto rectHeight = 10;
     
@@ -50,45 +50,24 @@ void ReverbPedal::paint (juce::Graphics& g)
     topRigth.setSize(rectWidth, rectHeight);
     topRigth.setCentre(sliderCol3CentreX, sliderRow1CentreY);
     
-    bottomLeft.setSize(rectWidth, rectHeight);
-    bottomLeft.setCentre(sliderCol1CentreX, sliderRow2CentreY);
-    
-    bottomRigth.setSize(rectWidth, rectHeight);
-    bottomRigth.setCentre(sliderCol3CentreX, sliderRow2CentreY);
-    
+    bottomCenter.setSize(rectWidth, rectHeight);
+    bottomCenter.setCentre(sliderCol2CentreX, sliderRow2CentreY);
+
     g.setColour(juce::Colours::white);
     g.drawEllipse(topLeft.toFloat(), 1.5f);
+    g.fillEllipse(topLeft.toFloat());
     
-    g.setColour(juce::Colours::red);
+    g.setColour(juce::Colours::white);
     g.drawEllipse(topRigth.toFloat(), 1.5f);
+    g.fillEllipse(topRigth.toFloat());
     
-    g.setColour(juce::Colours::blue);
-    g.drawEllipse(bottomLeft.toFloat(), 1.5f);
-    
-    g.setColour(juce::Colours::green);
-    g.drawEllipse(bottomRigth.toFloat(), 1.5f);
+    g.setColour(juce::Colours::white);
+    g.drawEllipse(bottomCenter.toFloat(), 1.5f);
+    g.fillEllipse(bottomCenter.toFloat());
+
     
     juce::Rectangle<float> led;
     led.setSize(7, 7);
     led.setCentre(sliderCol2CentreX, bypassSwitch.getBounds().getY() - 0.08f * getHeight());
     g.fillEllipse(led);
-}
-
-void ReverbPedal::resized()
-{
-    auto bounds         = getBounds();
-    auto width          = getWidth();
-    auto height         = getHeight();
-    sliderCol1CentreX   = getX() + width * 0.2f;
-    sliderRow1CentreY   = getY() + height * 0.2f;
-    sliderCol3CentreX   = getX() + width * 0.8f;
-    sliderCol2CentreX   = bounds.getCentreX();
-
-    sliderRow2CentreY   = bounds.getCentreY();
-    
-    auto buttonWidth    = 20;
-    auto buttonHeight   = 20;
-    bypassSwitch.setSize(buttonWidth, buttonHeight);
-    bypassSwitch.setCentrePosition(bounds.getCentreX(), bounds.getCentreY() + width * 0.6f);
-
 }
