a36313d5fa660a24d9f98a91f28eaebf6da012ea
Marina <92041588+marinahernandezsalas@users.noreply.github.com>
2023-05-14 18:39:11 +0200

    newplugin

---
+++ Source/PluginEditor còpia.cpp
@@ -0,0 +1,63 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+FootprintAudioProcessorEditor::FootprintAudioProcessorEditor (FootprintAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    
+    
+    // "Add and make visible" all child components of the editor
+    
+    addAndMakeVisible(displaySection);
+    addAndMakeVisible(pedalSection);
+    addAndMakeVisible(controlSection);
+    setSize (1200, 900);
+}
+
+FootprintAudioProcessorEditor::~FootprintAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void FootprintAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.setColour (juce::Colours::white);
+
+}
+
+void FootprintAudioProcessorEditor::resized()
+{
+    auto editorBounds       = getBounds();
+    auto editorCentre       = editorBounds.getCentre();
+    int editorWidth         = getWidth();
+    int editorHeight        = getHeight();
+    
+    int pedalSectionWidth   = 0.85f * editorWidth;
+    int pedalSectionHeight  = 0.4f * editorHeight;
+    int pedalYOffset        = editorHeight * 0.25f;
+    
+    pedalSection.setSize(pedalSectionWidth, pedalSectionHeight);
+    pedalSection.setCentrePosition(editorCentre.getX(),
+                                   editorCentre.getY() + pedalYOffset);
+    
+    int displaySectionWidth     = 0.6f * editorWidth;
+    int displaySectionHeight    = 0.35f * editorHeight;
+    auto pedalSectionTopRight   = pedalSection.getBounds().getTopRight();
+    
+    int displaySectionTopRightY = editorCentre.getY() - editorHeight * 0.45f;
+    int displaySectionTopRightX = pedalSectionTopRight.getX();
+    
+    displaySection.setSize(displaySectionWidth, displaySectionHeight);
+    displaySection.setTopRightPosition(displaySectionTopRightX, displaySectionTopRightY);
+}
