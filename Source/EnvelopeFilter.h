6c984b35f7f137a6f7d1e5aa36284e58d7544ed5
Bru Guell <bru.marcoi01@estudiant.upf.edu>
2023-05-15 21:28:08 +0200

    envelope

--- Source/EnvelopeFilter.h
+++ Source/EnvelopeFilter.h
@@ -9,3 +9,36 @@
 */
 
 #pragma once
+<<<<<<< Updated upstream
+=======
+#include "AudioProcessingModule.h"
+#include <JuceHeader.h>
+using Matrix = std::vector<std::vector<double>>;
+
+class EnvelopeFilter : AudioProcessingModule {
+
+public:
+    EnvelopeFilter();
+    ~EnvelopeFilter() override;
+    
+    void prepare(double sampleRate, int samplePerBlock) override;
+    void processBlock(juce::AudioBuffer<float>& buffer,
+        juce::MidiBuffer& midiMessages) override;
+
+    void setQualityFactor(float q) {
+        if (q >= 0) { qualityFactor = q; }
+    }
+
+    void setSensitivity(float s) {
+        if (s >= 0) { sensitivity = s; }
+    }
+
+
+    Matrix getLPFCoefficients(double sampleRate, float cutoffFrequency, float qualityFactor);
+    juce::AudioBuffer<float> getAmplitudeEnvelope(const juce::AudioBuffer<float>&buffer, double sampleRate);
+
+private:
+    float qualityFactor;
+    float sensitivity;
+};
+>>>>>>> Stashed changes
