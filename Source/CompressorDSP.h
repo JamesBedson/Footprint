a7dc9cfe481366b0c0dc1d7c01a3c68635d7fb0f
JamesBedsonUPF <84134199+JamesBedsonUPF@users.noreply.github.com>
2023-05-15 19:12:57 +0200

    Added compressor

---
+++ Source/CompressorDSP.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    CompressorDSP.h
+    Created: 15 May 2023 6:48:31pm
+    Author:  James Bedson
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "AudioProcessingModule.h"
+#include <JuceHeader.h>
+
+class Compressor : AudioProcessingModule {
+    
+public:
+    Compressor();
+    ~Compressor() override;
+    
+    void prepare(double sampleRate, int samplesPerBlock) override;
+    void processBlock(juce::AudioBuffer<float> &buffer, juce::MidiBuffer& midiMessages) override;
+
+private:
+    
+};
+
+
+
