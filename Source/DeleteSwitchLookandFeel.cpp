22576a5aab11e29e461e1764681912d54b225a15
Iker Landarech Mayo <iker.landarech01@estudiant.upf.edu>
2023-06-06 11:16:44 +0200

    DeleteSwitch Look & Feel Final Version

--- Source/DeleteSwitchLookandFeel.cpp
+++ Source/DeleteSwitchLookandFeel.cpp
@@ -26,10 +26,17 @@ void DeleteSwitchLookandFeel::drawToggleButton(juce::Graphics& g, juce::ToggleBu
     g.setColour(juce::Colours::white);
     g.fillRoundedRectangle(roundedRectangleButton, 9.f);
 
-    auto n = 0;
-    juce::Line<float> diagLine1 (juce::Point<float>((roundedRectangleButton.getX()) + n, roundedRectangleButton.getY() + n), juce::Point<float>((roundedRectangleButton.getX()) + roundedRectangleButton.getWidth() - n, roundedRectangleButton.getY() + roundedRectangleButton.getHeight() - n));
-    juce::Line<float> diagLine2(juce::Point<float>((roundedRectangleButton.getX()) + roundedRectangleButton.getWidth() + n, roundedRectangleButton.getY() + n), juce::Point<float>((roundedRectangleButton.getX() - n), roundedRectangleButton.getY() + roundedRectangleButton.getHeight() - n));
-    g.setColour(juce::Colours::white.darker());
-    g.drawLine(diagLine1, 1.0f);
-    g.drawLine(diagLine2, 1.0f);
+    int centerX = roundedRectangleButton.getWidth() / 2;
+    int centerY = roundedRectangleButton.getHeight() / 2;
+    int radius = std::min(roundedRectangleButton.getWidth(), roundedRectangleButton.getHeight()) / 2;
+    int startX = centerX - radius;
+    int startY = centerY - radius;
+    int endX = centerX + radius;
+    int endY = centerY + radius;
+
+    g.setColour(juce::Colours::white.darker().darker());
+
+    float n = 1.1f;
+    g.drawLine(startX + n, startY + n, endX - n, endY - n);
+    g.drawLine(startX + n, endY - n, endX - n, startY + n);
 }
